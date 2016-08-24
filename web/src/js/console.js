2b9ac14763c5783d8cfce295a8f956468c61ac76
Sung Kim <kr.dev.sk@gmail.com>
2016-08-24 18:07:12 -0400

    save/retrieve from firebase complete. Additional columns added.

--- web/src/js/console.js
+++ web/src/js/console.js
@@ -8,14 +8,18 @@ var config = {
     storageBucket: "tecforever-549d2.appspot.com",
 };
 firebase.initializeApp(config);
+var fbInventoryRef = firebase.database().ref('inventory');
+
 window.onload = function() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (!user) {
 			window.location.href = "login.html";
 	    }
 	});
+	fullInventory();
 };
 
+var feedData;
 var fileInputTextDiv = document.getElementById('file_input_text_div');
 var fileInput = document.getElementById('file_input_file');
 var fileInputText = document.getElementById('file_input_text');
@@ -25,11 +29,14 @@ fileInput.addEventListener('change', changeInputText);
 fileInput.addEventListener('change', changeState);
 fileInput.addEventListener('change', handleFile, false);
 
+confirmBtn.addEventListener('click', function() {
+	if (feedData != null) {
+		pushNewInventory(feedData);
+		reset();
+	}
+})
 cancelBtn.addEventListener('click', function() {
-	fileInput.value = "";
-	fileInputText.value = "";
-	changeState();
-	renderDOM([]);
+	reset();
 });
 
 $(window).scroll(function() {
@@ -46,6 +53,40 @@ function changeState() {
     	confirmBtn.classList.add('is-hidden');
   		cancelBtn.classList.add('is-hidden');
   	}
+}
+
+function reset() {
+	fileInput.value = "";
+	fileInputText.value = "";
+	changeState();
+	renderDOM(fullInventory());
+}
+
+function fullInventory(filter) {
+	var inventory = [];
+	if (filter) {
+		alert(filter + " filter detected.");
+	} else {
+		fbInventoryRef.once('value', function(snapshot) {
+			snapshot.forEach(function(item) {
+				var newItem = item.val();
+				newItem.firebaseKey = item.key;
+				inventory.push(newItem);
+			});
+			renderDOM(inventory);
+		});
+	}
+}
+
+function pushNewInventory(inventory) {
+	var updates = {};
+	var today = new Date();
+	for (var index in inventory) {
+		var newKey = fbInventoryRef.push().key;
+	    inventory[index].datePosted = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
+	    fbInventoryRef.child(newKey).set(inventory[index]);
+	}
+	alert("[" + today + "]\n\n" + fileInputText.value + "\n\tadded to the inventory.");
 }
 
 function changeInputText() {
@@ -77,65 +118,80 @@ function handleFile(e) {
 function parseXLSX(workbook) {
 	workbook.SheetNames.forEach(function(sheetName) {
 	    var worksheet = workbook.Sheets[sheetName];
-        var jsonData = XLSX.utils.sheet_to_json(worksheet);
-        renderDOM(jsonData);
+	    var rawJSON = XLSX.utils.sheet_to_json(worksheet);
+        feedData = processRawJsonData(rawJSON);
+        renderDOM(feedData);
 	});
 }
 
-/*
- * React DOM render using JSX
- */
-var NewInventoryList = React.createClass({
-	render: function() {
-		var itemNodes = this.props.data.map(function(item) {
-			return (
-				<NewItem data={item} />
-			);
-		});
-		return (
-			<div className="new_inventory_list">
-				{itemNodes}
-			</div>
-		);
-	}
-});
-
-var NewItem = React.createClass({
-	render: function() {
-		var data = this.props.data;
+function processRawJsonData(json) {
+	var processedJSON = [];
+	for (var i in json) {
+		var data = json[i];
 		if (!('MODEL#' in data)) {
-			return null;
+			continue;
 		}
 
 		var renderData = {};
-		var k;
-		for (k in data) {
+		for (var k in data) {
 			var key = k.toLowerCase();
 			if (key.includes("shipment")) {
 				renderData.shipment = data[k];
 			} else if (key.includes("packing") && key.includes("slips") && key.includes("seq")) {
 				renderData.packingSlipSeq = data[k];
+			} else if (key.includes("input") && key.includes("type")) {
+				renderData.sysInputType = data[k];
 			} else if (key.includes("lg") && key.includes("wh")) {
 				renderData.lgwh = data[k];
+			} else if (key.includes("wh") && key.includes("loc")) {
+				renderData.whLoc = data[k];
+			} else if (key.includes("cyn") && key.includes("wh")) {
+				renderData.cynWH = data[k];
+			} else if (key.includes("manufacture")) {
+				renderData.manufacturer = data[k];
+			} else if (key.includes("division")) {
+				renderData.division = data[k];
+			} else if (key.includes("bay") && key.includes("dock")) {
+				renderData.bayDockNum = data[k];
 			} else if (key.includes("inventory") && key.includes("type")) {
 				renderData.inventoryType = data[k];
 			} else if (key.includes("model") && key.includes("#")) {
-				renderData.model = data[k];
-				if ('MODEL' in data) {
+				if (key.includes("corrected")) {
+					renderData.correctedModel = data[k];
+				} else if ('MODEL' in data) {
 					renderData.model = data["MODEL"];
+				} else {
+					renderData.model = data[k];
 				}
 			} else if (key.includes("desc")) {
 				renderData.desc = data[k];
 			} else if (key.includes("serial") && key.includes("#")) {
 				renderData.serial = data[k];
+				if (key.includes("cyn")) {
+					renderData.cynSerial = data[k];
+				} else if (key.includes("corrected")) {
+					renderData.correctedSerial = data[k];
+				} else {
+					renderData.serialNum = data[k];
+				}
+			} else if (key.includes("tag") && key.includes("removal")) {
+				renderData.tagRemoval = data[k];
 			} else if (key.includes("cosmetic") && key.includes("grade")) {
 				renderData.grade = data[k];
 			} else if (key.includes("function") && key.includes("fail")) {
 				renderData.funcTestFailed = data[k];
-			} else if (key.includes("rec") && key.includes("q") && !key.includes("no")) {
+			} else if (key.includes("rec'd q'ty") && !(key.includes("no"))) {
 				renderData.recdQty = data[k];
-			} else if (key.includes("rec") && key.includes("q") && key.includes("no")) {
+			} else if (key.includes("rec'd q'ty") && key.includes("no")) {
 				renderData.noRecdQty = data[k];
+			} else if (key.includes("rec'd") && key.includes("truck") && key.includes("load") && key.includes("#")) {
+				renderData.recdTruckLoadNum = data[k];
+			} else if (key.includes("packing") && key.includes("slip")) {
+				if (key.includes("q'ty")) {
+					renderData.packingSlipQty = data[k];
+				} else if (key.includes("ship") && key.includes("#")) {
+					renderData.packingSlipNum = data[k];
+				}
 			} else if (key.includes("ship") && key.includes("truck")) {
 				renderData.truckLoadNum = data[k];
 			} else if (key.includes("hold")) {
@@ -146,6 +202,22 @@ var NewItem = React.createClass({
 				renderData.deliveryStorage = data[k];
 			} else if (key.includes("warranty")) {
 				renderData.warranty = data[k];
+			} else if (key.includes("prev") && key.includes("loc")) {
+				renderData.prevLoc = data[k];
+			} else if (key.includes("container") && key.includes("#")) {
+				renderData.containerNum = data[k];
+			} else if (key.includes("rec'd") && key.includes("date")) {
+				renderData.recdDate = data[k];
+			} else if (key.includes("rec'd") && key.includes("condition")) {
+				renderData.condition = data[k];
+			} else if (key.includes("purchased") && key.includes("pricing") && key.includes("rate")) {
+				renderData.purchasedPricingRate = data[k];
+			} else if (key.includes("unit") && key.includes("price")) {
+				renderData.unitPrice = data[k];
+			} else if (key.includes("lg") && key.includes("amount")) {
+				renderData.lgAmount = data[k];
+			} else if (key.includes("dae") && key.includes("heung")) {
+				renderData.daeheung = data[k];
 			} else if (key.includes("cyn") && (key.includes("cost") || key.includes("$"))) {
 				renderData.cynCost = data[k];
 			} else if (key.includes("vip")) {
@@ -170,36 +242,83 @@ var NewItem = React.createClass({
 				renderData.remain = data[k];
 			}
 		}
+		processedJSON.push(renderData);
+	}
+	return processedJSON;
+}
+
+/*
+ * React DOM render using JSX
+ */
+var NewInventoryList = React.createClass({
+	render: function() {
+		var itemNodes = this.props.data.map(function(item) {
+			return (
+				<NewItem data={item} />
+			);
+		});
+		return (
+			<div className="new_inventory_list">
+				{itemNodes}
+			</div>
+		);
+	}
+});
+
+var NewItem = React.createClass({
+	render: function() {
+		var data = this.props.data;
 		return (
 			<div className="new_inventory_item">
 			<table><tr>
-				<td><div className="shipment">{renderData.shipment}</div></td>
-				<td><div className="packing-slips-seq">{renderData.packingSlipSeq}</div></td>
-				<td><div className="lg-wh">{renderData.lgwh}</div></td>
-				<td><div className="inventory-type">{renderData.inventoryType}</div></td>
-				<td><div className="model-num">{renderData.model}</div></td>
-				<td><div className="model-desc">{renderData.desc}</div></td>
-				<td><div className="cyn-serial">{renderData.serial}</div></td>
-				<td><div className="cyn-cosmetic-grade">{renderData.grade}</div></td>
-				<td><div className="cyn-func-test-failed">{renderData.funcTestFailed}</div></td>
-				<td><div className="recd-qty">{renderData.recdQty}</div></td>
-				<td><div className="no-recd-qty">{renderData.noRecdQty}</div></td>
-				<td><div className="ship-truck-load-num">{renderData.truckLoadNum}</div></td>
-				<td><div className="hold-not-sold">{renderData.hold}</div></td>
-				<td><div className="offer">{renderData.offer}</div></td>
-				<td><div className="delivery-storage">{renderData.deliveryStorage}</div></td>
-				<td><div className="warranty">{renderData.warranty}</div></td>
-				<td><div className="cyn-cost">{renderData.cynCost}</div></td>
-				<td><div className="vip-cost">{renderData.vipCost}</div></td>
-				<td><div className="wholesale-cost">{renderData.wholesaleCost}</div></td>
-				<td><div className="original-market-cost">{renderData.origMarketCost}</div></td>
-				<td><div className="price-off-rate">{renderData.priceOffRate}</div></td>
-				<td><div className="retail-cost">{renderData.retailCost}</div></td>
-				<td><div className="sold-out-act-cost">{renderData.soldOutActCost}</div></td>
-				<td><div className="invoice-num">{renderData.invoice}</div></td>
-				<td><div className="tf-po-num">{renderData.tfPO}</div></td>
-				<td><div className="sold-out">{renderData.soldOut}</div></td>
-				<td><div className="remain">{renderData.remain}</div></td>
+				<td><div className="shipment">{data.shipment}</div></td>
+				<td><div className="packing-slips-seq">{data.packingSlipSeq}</div></td>
+				<td><div className="sys-input-type">{data.sysInputType}</div></td>
+				<td><div className="lg-wh">{data.lgwh}</div></td>
+				<td><div className="wh-loc">{data.whLoc}</div></td>
+				<td><div className="cyn-wh">{data.cynWH}</div></td>
+                <td><div className="manufacture">{data.manufacturer}</div></td>
+                <td><div className="division">{data.division}</div></td>
+                <td><div className="lg-bay-dock">{data.bayDockNum}</div></td>
+				<td><div className="inventory-type">{data.inventoryType}</div></td>
+				<td><div className="model-num">{data.model}</div></td>
+				<td><div className="model-desc">{data.desc}</div></td>
+				<td><div className="corrected-model">{data.correctedModel}</div></td>
+                <td><div className="serial-num">{data.serialNum}</div></td>
+                <td><div className="tag-removal">{data.tagRemoval}</div></td>
+                <td><div className="corrected-serial">{data.correctedSerial}</div></td>
+				<td><div className="cyn-serial">{data.cynSerial}</div></td>
+				<td><div className="cyn-cosmetic-grade">{data.grade}</div></td>
+				<td><div className="cyn-func-test-failed">{data.funcTestFailed}</div></td>
+				<td><div className="recd-qty">{data.recdQty}</div></td>
+				<td><div className="no-recd-qty">{data.noRecdQty}</div></td>
+				<td><div className="packing-slip-qty">{data.packingSlipQty}</div></td>
+                <td><div className="ship-load-packing-slip">{data.packingSlipNum}</div></td>
+				<td><div className="ship-truck-load-num">{data.truckLoadNum}</div></td>
+				<td><div className="hold-not-sold">{data.hold}</div></td>
+				<td><div className="offer">{data.offer}</div></td>
+				<td><div className="delivery-storage">{data.deliveryStorage}</div></td>
+				<td><div className="warranty">{data.warranty}</div></td>
+				<td><div className="cyn-prev-wh-loc">{data.prevLoc}</div></td>
+                <td><div className="recd-truck-load-num">{data.truckLoadNum}</div></td>
+                <td><div className="recd-container-num">{data.containerNum}</div></td>
+                <td><div className="recd-date">{data.recdDate}</div></td>
+                <td><div className="condition">{data.condition}</div></td>
+                <td><div className="purchased-pricing-rate">{data.purchasedPricingRate}</div></td>
+                <td><div className="lg-unit-price">{data.unitPrice}</div></td>
+                <td><div className="lg-amount">{data.lgAmount}</div></td>
+                <td><div className="daeheung">{data.daeheung}</div></td>
+				<td><div className="cyn-cost">{data.cynCost}</div></td>
+				<td><div className="vip-cost">{data.vipCost}</div></td>
+				<td><div className="wholesale-cost">{data.wholesaleCost}</div></td>
+				<td><div className="original-market-cost">{data.origMarketCost}</div></td>
+				<td><div className="price-off-rate">{data.priceOffRate}</div></td>
+				<td><div className="retail-cost">{data.retailCost}</div></td>
+				<td><div className="sold-out-act-cost">{data.soldOutActCost}</div></td>
+				<td><div className="invoice-num">{data.invoice}</div></td>
+				<td><div className="tf-po-num">{data.tfPO}</div></td>
+				<td><div className="sold-out">{data.soldOut}</div></td>
+				<td><div className="remain">{data.remain}</div></td>
 			</tr></table>
 			</div>
 		)
@@ -207,7 +326,6 @@ var NewItem = React.createClass({
 });
 
 function renderDOM(data) {
-	console.log(data);
 	ReactDOM.render(
 		<NewInventoryList data={data} />,
 		document.getElementById('inventory_container')
