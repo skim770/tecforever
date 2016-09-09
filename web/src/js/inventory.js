6ca872f8ca3594f0362ba5465b25c3fce225696e
Sung Kim <kr.dev.sk@gmail.com>
2016-09-09 02:56:42 -0400

    filter ui

--- web/src/js/inventory.js
+++ web/src/js/inventory.js
@@ -12,6 +12,7 @@ firebase.auth().signInAnonymously().catch(function(error) {
   var errorMessage = error.message;
 });
 
+var xmlHttpReq = new XMLHttpRequest();
 var categories = ['#new', '#ref', '#wd', '#dw', '#cooking', '#av', '#tv'];
 var itemsNew = [];
 var itemsRef = [];
@@ -53,7 +54,7 @@ inventoryRef.once('value').then(function(snapshot) {
 			var type = item.val().inventoryType.toUpperCase();
 			if (type.includes("RF") || type.includes("REF")) {
 				itemsRef.push(item.val());
-			} else if (type.includes("WASH") || type.includes("DRYER")) {
+			} else if (type.includes("WASH") || type.includes("DRYER") || type.includes("PEDESTAL")) {
 				itemsWd.push(item.val());
 			} else if (type.includes("DISHW")) {
 				itemsDw.push(item.val());
@@ -61,7 +62,7 @@ inventoryRef.once('value').then(function(snapshot) {
 				itemsCooking.push(item.val());
 			} else if (type.includes("DVD") || type.includes("SOUND") || type.includes("AUDIO") || type.includes("PROJECTOR") || type.includes("THEATER")) {
 				itemsAv.push(item.val());
-			} else if (type.includes("MONITOR") || type.includes("TV")) {
+			} else if (type.includes("TV")) {
 				itemsTv.push(item.val());
 			} else {
 				itemsOthers.push(item.val());
@@ -72,10 +73,19 @@ inventoryRef.once('value').then(function(snapshot) {
 
 function activateCategory(id) {
 	categories.forEach(function(category) {
+		if (id === '#new' || id === '#others') {
+			$('#sidebar-wrapper').addClass("none");
+			$('#page-content-wrapper').addClass("scoot-left");
+		} else {
+			$('#sidebar-wrapper').removeClass("none");
+			$('#page-content-wrapper').removeClass("scoot-left");
+		}
 		if (id === category) {
 			$(category).addClass('active');
+			$(category + "-filters").removeClass('is-hidden');
 		} else {
 			$(category).removeClass('active');
+			$(category + "-filters").addClass('is-hidden');
 		}
 	});
 	switch(id) {
@@ -126,15 +136,17 @@ var Item = React.createClass({
 		var type = this.props.data.inventoryType.toUpperCase();
 		if (type.includes("RF") || type.includes("REF")) {
 			imgTag = (<img src="resources/img/appliance_icons/refrigerator.svg" alt="Refrigerator" height="100%"/>);
-		} else if (type.includes("WASH") || type.includes("DRYER")) {
+		} else if (type.includes("WASH") || type.includes("DRYER") || type.includes("PEDESTAL")) {
 			imgTag = (<img src="resources/img/appliance_icons/washing-machine.svg" width="100%" height="100%"/>);
 		} else if (type.includes("DISHW")) {
+			// var srclink = "http://www.lg.com/us/images/dishwashers/" + this.props.data.model.toLowerCase() + "/gallery/large01.jpg";
+			// imgTag = (<img src={srclink} alt="Image not found" height="200px"/>);
 			imgTag = (<img src="resources/img/appliance_icons/dishwasher.svg" width="100%" height="100%"/>);
 		} else if (type.includes("RANG") || type.includes("COOKTOP") || type.includes("OVEN") || type.includes("STOVE") || type.includes("MICRW")) {
 			imgTag = (<img src="resources/img/appliance_icons/oven.svg" width="100%" height="100%"/>);
 		} else if (type.includes("DVD") || type.includes("SOUND") || type.includes("AUDIO") || type.includes("PROJECTOR") || type.includes("THEATER")) {
 			imgTag = (<img src="resources/img/appliance_icons/music-player.svg" width="100%" height="100%"/>);
-		} else if (type.includes("MONITOR") || type.includes("TV")) {
+		} else if (type.includes("TV")) {
 			imgTag = (<img src="resources/img/appliance_icons/television.svg" width="100%" height="100%"/>);
 		} else {
 			imgTag = (<img src="resources/img/tf-logo.png" width="100%" height="100%"/>);
